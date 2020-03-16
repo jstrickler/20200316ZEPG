ab7b3d0518ec975535f8bf51a81be7106a5b9f93
jstrickler <jstrickler@gmail.com>
2020-03-16 16:40:49 -0400

    End of Chapter 2

---
+++ cmd_line_args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+
+print(sys.argv)
+
+print("script name:", sys.argv[0])
+
+print("first arg:", sys.argv[1])
+
+print("second arg:", sys.argv[2])
---
+++ reading_input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+color = input("What is your favorite color? ")
+
+print("I like {}".format(color))
+
+value = float(input("What is the value? "))  # read STDIN
+
+print(value)
